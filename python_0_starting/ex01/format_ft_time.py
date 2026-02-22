c8d20fc6a22fd2ebb80dbeb009d3d7d980646781
nivicius-42 <niviciusdev@gmail.com>
2026-02-22 16:54:16 -0300

    Format timestamp string for better readability

--- python_0_starting/ex01/format_ft_time.py
+++ python_0_starting/ex01/format_ft_time.py
@@ -1,9 +1,11 @@
 import time
 from datetime import datetime
 
+
 def format_ft_time() -> tuple[str, str]:
     timestamp = time.time()
-    timestamp_string = f"Seconds since January 1, 1970: {timestamp:,.4f} or {timestamp:.2e} in scientific notation"
+    timestamp_string = f"Seconds since January 1, 1970: {timestamp:,.4f} \
+        or {timestamp:.2e} in scientific notation"
     date_str = datetime.now().strftime("%b %d %Y")
     return timestamp_string, date_str
 
