8a8352a4956707d77f87979657a399e9ef3eac6a
vinicius <niviciusdev@gmail.com>
2026-03-21 14:11:31 -0300

    feat: Add OOP examples with Baratheon and Lannister classes
    Introduce `S1E7.py` with `Baratheon` and `Lannister` classes extending `Character`. Implement custom attributes and methods for both families. Add `tester.py` to validate functionality.

---
+++ python_3_oop/ex01/S1E9.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class Character(ABC):
+    """Abstract class for characters."""
+
+    def __init__(self, first_name: str, is_alive: Optional[bool] = True):
+        """Initialize a Character instance."""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    def health_state(self):
+        """Set the character's health state to False."""
+        self.is_alive = False
+
+    @abstractmethod
+    def die(self):
+        """Method to kill the character."""
+        pass
+
+    def __str__(self):
+        """Return a string representation of the Character instance."""
+        attrs = tuple(
+            value for key, value in vars(self).items()
+            if key not in ("first_name", "is_alive")
+        )
+        return f"Vector: {attrs}"
+
+    def __repr__(self):
+        """Return a string representation of the Character instance."""
+        return self.__str__()
+
+
+class Stark(Character):
+    """Class that inherits from Character and represents a Stark character."""
+
+    def __init__(self, first_name: str, is_alive: Optional[bool] = True):
+        """Initialize a Stark instance."""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Kill the character."""
+        self.health_state()
