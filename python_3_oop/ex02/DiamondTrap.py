d1c5b590a4941ad370e2da21ab3aeabdc994ee84
vinicius <niviciusdev@gmail.com>
2026-03-22 22:34:42 -0300

    feat: Add OOP examples with `King`, `Baratheon`, `Lannister`, and `Stark` classes
    Introduce new classes (`King`, `Baratheon`, `Lannister`, `Stark`) extending `Character` to demonstrate inheritance, abstraction, and method overriding. Add `tester.py` to validate functionality.

---
+++ python_3_oop/ex02/DiamondTrap.py
@@ -0,0 +1,64 @@
+from typing import Optional
+
+from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    """A class representing a fake King."""
+
+    def __init__(self, first_name: str, is_alive: Optional[bool] = True) -> None:
+        """
+        Initializes a new instance of the class with given attributes.
+
+        Args:
+            first_name (str): The first name of the individual.
+            is_alive (Optional[bool]): Status indicating if the individual is alive. Defaults to True.
+        """
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def set_eyes(self, eyes: str) -> None:
+        """
+        Sets the value of the eyes attribute.
+
+        Args:
+            eyes (str): The eye color or description to be set.
+        """
+        self.eyes = eyes
+
+    def set_hairs(self, hairs: str) -> None:
+        """
+        Sets the number or type of hairs for the object.
+
+        This method allows the modification or updating of the 'hairs' attribute
+        of the object. It is intended to set a specific number or description of
+        hairs based on the provided input.
+
+        Args:
+            hairs (str): The number or description of hairs to set for the object.
+        """
+        self.hairs = hairs
+
+    def get_eyes(self) -> str:
+        """
+        Gets the value of the eyes attribute.
+
+        Returns:
+            str: The eye color or description.
+        """
+        return self.eyes
+
+    def get_hairs(self) -> str:
+        """
+        Gets the value of the hairs attribute.
+
+        Returns:
+            str: The number or description of hairs.
+        """
+        return self.hairs
+
+    def die(self) -> None:
+        """Kill the character."""
+        self.health_state()
---
+++ python_3_oop/ex02/S1E7.py
@@ -0,0 +1,78 @@
+from typing import Optional
+
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """
+    Represents a member of the Baratheon family.
+
+    The Baratheon class is a subclass of Character, representing individuals belonging to
+    the Baratheon family. Each Baratheon has specific characteristics such as
+    a family name, eye color, and hair color. This class provides functionality to
+    handle the character's death and string representations.
+
+    Attributes:
+        family_name (str): The family name of the character, default is "Baratheon".
+        eyes (str): The eye color of the character, default is "brown".
+        hairs (str): The hair color of the character, default is "dark".
+    """
+
+    def __init__(self, first_name: str, is_alive: Optional[bool] = True) -> None:
+        """Initialize a Baratheon instance."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self) -> None:
+        """Kill the character."""
+        self.health_state()
+
+    def __str__(self):
+        """Return a string representation of the Baratheon instance."""
+        return super().__str__()
+
+    def __repr__(self):
+        """Return a string representation of the Baratheon instance."""
+        return super().__repr__()
+
+
+class Lannister(Character):
+    """A class representing a member of the Lannister family.
+
+    The Lannister class models a character from the Lannister family,
+    extending the general `Character` class. It provides specific
+    attributes and functionality related to this noble family in a
+    fictional setting.
+
+    Attributes:
+        family_name (str): The family name of the character, which is
+            always "Lannister" for instances of this class.
+        eyes (str): The eye color of the character, which is "blue".
+        hairs (str): The hair color of the character, which is "light".
+    """
+
+    def __init__(self, first_name: str, is_alive: Optional[bool] = True) -> None:
+        """Initialize a Lannister instance."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self) -> None:
+        """Kill the character."""
+        self.health_state()
+
+    @staticmethod
+    def create_lannister(first_name: str, is_alive: bool = True) -> "Lannister":
+        """Create a Lannister instance."""
+        return Lannister(first_name, is_alive)
+
+    def __str__(self):
+        """Return a string representation of the Lannister instance."""
+        return super().__str__()
+
+    def __repr__(self):
+        """Return a string representation of the Lannister instance."""
+        return super().__repr__()
